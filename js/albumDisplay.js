f44d80cfbb5b1af437de5fbd1ad57e720abed3c1
gr1ll0o <thiagotobias.grillo@gmail.com>
2026-07-19 20:56:11 -0300

    update: URL params & search system added

--- js/albumDisplay.js
+++ js/albumDisplay.js
@@ -1,10 +1,32 @@
-// genders: jazz, rock, altrock, punk, postpunk, arg, fusion, prog, 70s, 80s, 90s, 00s
+// genres: jazz, rock, altrock, punk, postpunk, arg, fusion, prog, 70s, 80s, 90s, 00s
 
-let container = document.getElementById("container");
+const container = document.getElementById("container");
+const searchInput = document.getElementById("search");
 
-async function loadAlbums() {
+async function loadAlbumsURL(mode=0) { // 0: URL Params, 1: Search params
+
     const response = await fetch("albums.json");
-    const albums = await response.json();
+    let albums = await response.json();
+
+    if (mode == 0) {
+        const params = new URLSearchParams(window.location.search);
+        const genre = params.get("genre");
+
+        if (genre) {
+            albums = albums.filter(album =>
+                album.genres.includes(genre)
+            );
+        }
+    }else if (mode == 1) {
+        const name = searchInput.value;
+
+        albums = albums.filter(album => 
+            album.title.includes(name)
+        );
+        container.innerHTML = ``;
+        console.log(albums);
+        if (albums == "") container.innerHTML = `<p>No hay resultados.</p>`;
+    }
 
     albums.forEach(album => {
         container.innerHTML += `
@@ -13,14 +35,19 @@ async function loadAlbums() {
                     <div style="display:flex; justify-content:center;">
                         <img src="${album.cover}" alt="${album.title}">
                     </div>
-                    <div style="display: flex; justify-content: space-between;">
+
+                    <div style="display:flex; justify-content:space-between;">
                         <h1>${album.title}</h1>
                         <h1>${album.rating}</h1>
                     </div>
-                    <div style="display: flex; justify-content: space-between;">
+
+                    <div style="display:flex; justify-content:space-between;">
                         <h3>${album.artist} - ${album.year}</h3>
-                        <h3 style="margin-right: 12px;">${album.genders[0]}</h3>
+                        <h3 style="margin-right:12px;">
+                            ${album.genres[0]}
+                        </h3>
                     </div>
+
                     <p>${album.description}</p>
                 </div>
             </a>
@@ -28,4 +55,10 @@ async function loadAlbums() {
     });
 }
 
-loadAlbums();
+searchInput.addEventListener('keydown', (e) => {
+    if (event.key === "Enter") {
+        loadAlbumsURL(1);
+    }
+});
+
+loadAlbumsURL(0);
